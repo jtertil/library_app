c29c1f73c7f9758a59536be71d6d2787663e7c77
jtertil <jtertil@gmail.com>
2018-09-17 17:12:22 +0200

    list and details pages for books and authors

--- catalog/models.py
+++ catalog/models.py
@@ -16,34 +16,35 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+from django.shortcuts import reverse #Jt. (!!) Without this line --> NameError (name 'reverse' is not defined), when I try go to /catalog/books/ URL
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
-    # Foreign Key used because book can only have one author Jt.(!!), but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file.
+        # Foreign Key used because book can only have one author Jt.(!!), but authors can have multiple books
+        # Author as a string rather than object because it hasn't been declared yet in the file.
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ManyToManyField(Language, help_text='Select a language for this book')
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.title
-
-    def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
+        # ManyToManyField used because genre can contain many books. Books can cover many genres.
+        # Genre / Language class has already been defined so we can specify the object above.
 
     def display_genre(self):
         """Create a string for the Genre. This is required to display genre in Admin."""
         return ', '.join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre' #Jt. (??)
 
-        display_genre.short_description = 'Genre'
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular book instance.
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
 
 
 import uuid # Required for unique book instances
